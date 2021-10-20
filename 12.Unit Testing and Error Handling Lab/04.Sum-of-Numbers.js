178474ac04e92ff63f980fda39e15b817a2c5f53
Reneta <renkagh55@gmail.com>
2021-10-20 15:07:57 +0300

    ?

--- 12.Unit Testing and Error Handling Lab/04.Sum-of-Numbers.js
+++ 12.Unit Testing and Error Handling Lab/04.Sum-of-Numbers.js
@@ -11,4 +11,3 @@ function sum(arr) {
 // Your tests will be supplied with a function named &#39;sum()&#39;. It should meet the following requirements:
 //  Take an array of numbers as argument
 //  Return the sum of the values of all elements inside the array
-//ihjhiohoihio
