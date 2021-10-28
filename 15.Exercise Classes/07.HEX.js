4a917026535d26a90b47167d058f08c0b0fa03a6
Reneta <renkagh55@gmail.com>
2021-10-28 13:08:30 +0300

    --

--- 15.Exercise Classes/07.HEX.js
+++ 15.Exercise Classes/07.HEX.js
@@ -1,8 +1,33 @@
 class Hex {
-  // TODO: implement this class...
-  constructor(value) {}
+  constructor(value) {
+    this.value = value;
+  }
+  valueOf() {
+    return this.value;
+  }
+  toString() {
+    return `0x${this.value.toString(16).toUpperCase()}`;
+  }
+  plus(num) {
+    let result = this.value + Number(num.valueOf());
+    return new Hex(result);
+  }
+  minus(num) {
+    let result = this.value - Number(num.valueOf());
+    return new Hex(result);
+  }
+  parse(text) {
+    return parseInt(text, 16);
+  }
 }
-
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString() === "0xF");
+console.log(FF.parse("AAA"));
 // Write a class Hex, having the following functionality:
 //  The constructor takes one parameter value, which is a number
 //  valueOf() This function should return the value property of the hex class.
