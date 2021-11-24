4451096f49edb97e6bc82a2504530f91634c0095
Reneta <renkagh55@gmail.com>
2021-11-24 13:11:17 +0200

    --

--- 12.Unit Testing and Error Handling Lab/exercise/06.Dynamic-Validation/app.js
+++ 12.Unit Testing and Error Handling Lab/exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+  let inputElement = document.querySelector("#email");
+  let emailRegex = /(^[a-z]+@[a-z]+\.[a-z]+$)/;
+
+  inputElement.addEventListener("change", () => {
+    if (!emailRegex.test(inputElement.value)) {
+      inputElement.classList.add("error");
+    } else {
+      inputElement.classList.remove("error");
+    }
+  });
 }
